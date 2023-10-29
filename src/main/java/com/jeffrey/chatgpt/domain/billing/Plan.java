e0b181cf32dc0c7048b058ed291b7c4bb8bed73b
JeffreyYou <52956161+JeffreyYou@users.noreply.github.com>
2023-10-28 18:59:08 -0700

    Update Billing

--- src/main/java/com/jeffrey/chatgpt/domain/billing/Plan.java
+++ src/main/java/com/jeffrey/chatgpt/domain/billing/Plan.java
@@ -3,10 +3,8 @@ package com.jeffrey.chatgpt.domain.billing;
 import lombok.Data;
 
 /**
- * @author 小傅哥，微信：fustack
- * @description 账单
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ * @author Jeffrey You
+ * @description Billing Plan
  */
 @Data
 public class Plan {
